63bee201a36949ef8a1a0dea8bfa5a122626e3a0
esmameral <esma.meral@gmail.com>
2024-11-05 22:39:52 +0300

    refactor

--- schoolapp/src/test/java/ozyegin/schoolapp/ApplicationTests.java
+++ schoolapp/src/test/java/ozyegin/schoolapp/ApplicationTests.java
@@ -8,9 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 
-import jakarta.transaction.Transactional;
 import ozyegin.schoolapp.model.Course;
 import ozyegin.schoolapp.model.Student;
 import ozyegin.schoolapp.model.Transcript;
